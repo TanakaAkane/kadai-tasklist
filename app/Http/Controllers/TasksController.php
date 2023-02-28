2725c60cedaccdea844d76da74042e30a5d8f208
hanako.suzuki <huazilingmu74@gmail.com>
2023-02-28 05:26:20 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,11 +46,14 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
         // メッセージを作成
         $task = new Task;
         $task->content = $request->content;
         $task->save();
-
         // トップページへリダイレクトさせる
         return redirect('/');
     }
@@ -100,6 +103,10 @@ class TasksController extends Controller
 
 public function update(Request $request, $id)
 {
+    // バリデーション
+    $request->validate([
+    'content' => 'required|max:255',
+    ]);
     // idの値でメッセージを検索して取得
     $task = Task::findOrFail($id);
     // メッセージを更新
